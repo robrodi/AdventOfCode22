754b9121e61a34cc33e4c9ccdf14c15d250d48d8
Rob <robert.rodi@gmail.com>
2022-11-30 22:56:55 -0800

    linted

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,18 +5,20 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let cal_counts = group_lines(input);
-    let top3_sum: u32 = cal_counts[cal_counts.len()-3..].iter().sum();
+    let top3_sum: u32 = cal_counts[cal_counts.len() - 3..].iter().sum();
     Some(top3_sum)
 }
 fn group_lines(input: &str) -> Vec<u32> {
     // split into per-elf.
     let cal_lists = input.split("\n\n");
     //for each group of cals, convert each to int & sum
-    let mut sums = cal_lists.map(|list| list.lines().map(|cal| cal.parse::<u32>().unwrap()).sum()).collect::<Vec<_>>();
+    let mut sums = cal_lists
+        .map(|list| list.lines().map(|cal| cal.parse::<u32>().unwrap()).sum())
+        .collect::<Vec<_>>();
     sums.sort();
     sums
 }
-    // Consumes the itera
+// Consumes the itera
 fn main() {
     let input = &advent_of_code::read_file("inputs", 1);
     advent_of_code::solve!(1, part_one, input);
