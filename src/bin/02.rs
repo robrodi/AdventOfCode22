e3dbe94aac4259bbe2f926ea24e72c06757211f2
Rob <robert.rodi@gmail.com>
2022-12-01 23:26:33 -0800

    fmt,lint

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,74 +1,78 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    let scores: Vec<u32> = input.lines().map(|l|  score_l(l)).collect();
+    let scores: Vec<u32> = input.lines().map(score_l).collect();
     Some(scores.iter().sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let scores: Vec<u32>  = input.lines().map(|l| score2(l)).collect();
+    let scores: Vec<u32> = input.lines().map(score2).collect();
     Some(scores.iter().sum())
 }
-fn score2(l: &str) -> u32{
-    let opp = l.chars().nth(0).unwrap();
-    let outcome = l.chars().nth(2).unwrap();
-    match outcome{
+fn score2(l: &str) -> u32 {
+    let opp = l.chars().next().unwrap();
+    match l.chars().nth(2).unwrap() {
+        // outcome is char[2]
         'X' => lose_score(opp),
         'Y' => draw_score(opp),
         'Z' => win_score(opp),
-        _ => 0
+        _ => 0,
     }
 }
 
-fn draw_score (input: char) -> u32{
+fn draw_score(input: char) -> u32 {
     3 + match input {
         'A' => 1, // rock
         'B' => 2, // paper
         'C' => 3, // scissors
-        other => 0
+        _ => 0,
     }
 }
-fn win_score (input: char) -> u32{
+fn win_score(input: char) -> u32 {
     6 + match input {
         'A' => 2, // rock
         'B' => 3, // paper
         'C' => 1, // scissors
-        other => 0
+        _ => 0,
     }
 }
-fn lose_score (input: char) -> u32{
+fn lose_score(input: char) -> u32 {
     match input {
         'A' => 3, // rock
         'B' => 1, // paper
         'C' => 2, // scissors
-        other => 0
+        _ => 0,
     }
 }
-fn score_l(l: &str) -> u32{
-    score(l.chars().nth(0).unwrap(), l.chars().nth(2).unwrap())
+fn score_l(l: &str) -> u32 {
+    score(l.chars().next().unwrap(), l.chars().nth(2).unwrap())
 }
 fn score(opp: char, mine: char) -> u32 {
     let my_pick = match mine {
         'Y' => 2,
         'X' => 1,
         'Z' => 3,
-        other => 0
+        _ => 0,
     };
 
     let result: u32;
     if (opp == 'C' && mine == 'Z') || // tie scissor
         (opp == 'B' && mine == 'Y') || // tie rock
-        (opp == 'A' && mine == 'X'){ // tie paper
+        (opp == 'A' && mine == 'X')
+    {
+        // tie paper
         result = 3;
     }
     // 3 wins
     else if (opp == 'A' && mine == 'Y') || // rock, paper
         (opp == 'B' && mine == 'Z') || // Paper, Scissors
-        (opp == 'C' && mine == 'X') { // scissors, Rock
-            result = 6;
-    }
-    else{ // loss
+        (opp == 'C' && mine == 'X')
+    {
+        // scissors, Rock
+        result = 6;
+    } else {
+        // loss
         result = 0
     }
-    
+
     my_pick + result
 }
 
@@ -95,14 +99,14 @@ mod tests {
     }
 
     #[test]
-    fn test_score(){
+    fn test_score() {
         let s = 's';
         assert_eq!(score('A', 'Y'), 8, "Paper, Win");
         assert_eq!(score('B', 'X'), 1, "Rock, Loss");
         assert_eq!(score('C', 'Z'), 6, "scissor, tie");
     }
     #[test]
-    fn test_score_2(){
+    fn test_score_2() {
         assert_eq!(score2("A Y"), 4, "Rock, Draw");
         assert_eq!(score2("B X"), 1, "Paper, Loss");
         assert_eq!(score2("C Z"), 7, "scissor, Win");
