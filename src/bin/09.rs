7abc60eeae7b64082f63e76a29849828a419fec4
Rob <robert.rodi@gmail.com>
2022-12-10 21:49:28 -0800

    works

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -27,28 +27,32 @@ fn do_thing(input: &str, rope_length: usize) -> Option<u32> {
                 'R' => head.x += 1,
                 _ => panic!("WTF"),
             }
-            
-            let last = &head;
+            let mut current = head;
             for i in 0..rope_length{
-                let tail = knots[i];
+                let last = current;
+                // let mut last = if (i == 0) {&head} else {&knots[i -1]};
+
+                // pull a mutable referene to the knot we want to putz with.
+                let tail =  &mut knots[i];
                 // not adjacent
                 if (last.x - tail.x).abs() > 1 || (last.y - tail.y).abs() > 1{
                     if last.x != tail.x && last.y != tail.y { // move diagonally
-                        if last.x > tail.x { knots[i].x += 1; }
-                        if last.x < tail.x { knots[i].x -= 1; }
-                        if last.y > tail.y { knots[i].y += 1; }
-                        if last.y < tail.y { knots[i].y -= 1; }
+                        if last.x > tail.x { tail.x += 1; }
+                        if last.x < tail.x { tail.x -= 1; }
+                        if last.y > tail.y { tail.y += 1; }
+                        if last.y < tail.y { tail.y -= 1; }
                     }
                     else{
-                        if last.x - tail.x > 1 { knots[i].x += 1; }
-                        if tail.x - last.x > 1 { knots[i].x -= 1; }
-                        if last.y - tail.y > 1 { knots[i].y += 1; }
-                        if tail.y - last.y > 1 { knots[i].y -= 1; }
+                        if last.x - tail.x > 1 { tail.x += 1; }
+                        if tail.x - last.x > 1 { tail.x -= 1; }
+                        if last.y - tail.y > 1 { tail.y += 1; }
+                        if tail.y - last.y > 1 { tail.y -= 1; }
                     }
                 }
+                current = *tail;
             }
             
-            let mut last = knots.last().unwrap();//Point::new(0, 0);
+            let last = knots.last().unwrap();//Point::new(0, 0);
     // move tail 1 step towards head
             println!("Head: {:?} Tail: {:?}", head, last);
 
@@ -73,16 +77,8 @@ fn parse(input: &str) -> Vec<(char, i32)> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let moves = parse("R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20");
     
-    None
+    do_thing(input, 9)
 }
 
 fn main() {
@@ -104,6 +100,15 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        let other_input =
+"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+        assert_eq!(part_two(&other_input), Some(36));
     }
 }
