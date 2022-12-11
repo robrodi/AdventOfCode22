8cc7b1e2d9255f30bb079e2b09b49b0be733dd0a
Rob <robert.rodi@gmail.com>
2022-12-10 21:49:43 -0800

    fmt

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashSet};
 use itertools::Itertools;
+use std::collections::HashSet;
 use substring::Substring;
 type Point = euclid::Point2D<i32, i32>;
 
@@ -28,37 +28,52 @@ fn do_thing(input: &str, rope_length: usize) -> Option<u32> {
                 _ => panic!("WTF"),
             }
             let mut current = head;
-            for i in 0..rope_length{
+            for i in 0..rope_length {
                 let last = current;
                 // let mut last = if (i == 0) {&head} else {&knots[i -1]};
 
                 // pull a mutable referene to the knot we want to putz with.
-                let tail =  &mut knots[i];
+                let tail = &mut knots[i];
                 // not adjacent
-                if (last.x - tail.x).abs() > 1 || (last.y - tail.y).abs() > 1{
-                    if last.x != tail.x && last.y != tail.y { // move diagonally
-                        if last.x > tail.x { tail.x += 1; }
-                        if last.x < tail.x { tail.x -= 1; }
-                        if last.y > tail.y { tail.y += 1; }
-                        if last.y < tail.y { tail.y -= 1; }
-                    }
-                    else{
-                        if last.x - tail.x > 1 { tail.x += 1; }
-                        if tail.x - last.x > 1 { tail.x -= 1; }
-                        if last.y - tail.y > 1 { tail.y += 1; }
-                        if tail.y - last.y > 1 { tail.y -= 1; }
+                if (last.x - tail.x).abs() > 1 || (last.y - tail.y).abs() > 1 {
+                    if last.x != tail.x && last.y != tail.y {
+                        // move diagonally
+                        if last.x > tail.x {
+                            tail.x += 1;
+                        }
+                        if last.x < tail.x {
+                            tail.x -= 1;
+                        }
+                        if last.y > tail.y {
+                            tail.y += 1;
+                        }
+                        if last.y < tail.y {
+                            tail.y -= 1;
+                        }
+                    } else {
+                        if last.x - tail.x > 1 {
+                            tail.x += 1;
+                        }
+                        if tail.x - last.x > 1 {
+                            tail.x -= 1;
+                        }
+                        if last.y - tail.y > 1 {
+                            tail.y += 1;
+                        }
+                        if tail.y - last.y > 1 {
+                            tail.y -= 1;
+                        }
                     }
                 }
                 current = *tail;
             }
-            
-            let last = knots.last().unwrap();//Point::new(0, 0);
-    // move tail 1 step towards head
+
+            let last = knots.last().unwrap(); //Point::new(0, 0);
+                                              // move tail 1 step towards head
             println!("Head: {:?} Tail: {:?}", head, last);
 
             visited.insert(*last);
         }
-    
     }
     Some(visited.len() as u32)
 }
@@ -77,7 +92,6 @@ fn parse(input: &str) -> Vec<(char, i32)> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    
     do_thing(input, 9)
 }
 
@@ -100,8 +114,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        let other_input =
-"R 5
+        let other_input = "R 5
 U 8
 L 8
 D 3
